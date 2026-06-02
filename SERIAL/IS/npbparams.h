4ba18ffc8f3f9a87e47513f7bf90688879abe9da
anninyia <anninyia@gmail.com>
2026-06-02 19:40:20 +0000

    atualização dos arquivos, rm de inuteis

--- SERIAL/IS/npbparams.h
+++ SERIAL/IS/npbparams.h
@@ -1,10 +1,10 @@
-#define CLASS 'C'
+#define CLASS 'S'
 /*
    This file is generated automatically by the setparams utility.
    It sets the number of processors and the class of the NPB
    in this directory. Do not modify it by hand.   */
    
-#define COMPILETIME "14 Mar 2026"
+#define COMPILETIME "02 Jun 2026"
 #define NPBVERSION "3.3.1"
 #define CC "cc"
 #define CFLAGS "-O -mcmodel=medium"
